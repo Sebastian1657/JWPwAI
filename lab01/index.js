b53be1cbadc539cc5428b076bd98cbe8d8669488
honoryk <118126185+honoryk@users.noreply.github.com>
2025-10-06 11:43:26 +0200

    lab01 poprawione

--- lab01/index.js
+++ lab01/index.js
@@ -13,8 +13,8 @@ const server = http.createServer(function(request, response) {
 });
 
 server.listen(port, hostname, () => {
-    util.log(v8.getHeapStatistics());
+    util.debuglog(v8.getHeapStatistics());
     console.log(path.basename(__filename));
-    util.log(path.join(__dirname, 'uploads','images'));
+    util.debuglog(path.join(__dirname, 'uploads','images'));
     console.log(`Server running at http://${hostname}:${port}/`);
 });
